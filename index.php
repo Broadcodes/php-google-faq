8823c35e38ad55892da5a260c29ded54d87fba75
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-14 14:14:15 +0200

    Inzializzazione php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti – Privacy e termini – Google</title>
+
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <?php 
+
+        ?>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
